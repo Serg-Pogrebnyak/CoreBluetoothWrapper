9d3cbc1d43a11d26e76b9550e43d34912759b662
Sergey <pogrebnyak.vetal1996@gmail.com>
2021-06-17 11:33:26 +0300

    configured and added test print struct

---
+++ CoreBluetoothWrapper/CoreBluetoothWrapper/CoreBluetoothWrapper.h
@@ -0,0 +1,18 @@
+//
+//  CoreBluetoothWrapper.h
+//  CoreBluetoothWrapper
+//
+//  Created by Sergey Pohrebnuak on 17.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreBluetoothWrapper.
+FOUNDATION_EXPORT double CoreBluetoothWrapperVersionNumber;
+
+//! Project version string for CoreBluetoothWrapper.
+FOUNDATION_EXPORT const unsigned char CoreBluetoothWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreBluetoothWrapper/PublicHeader.h>
+
+
---
+++ CoreBluetoothWrapper/CoreBluetoothWrapper/CoreBluetoothWrapper.swift
@@ -0,0 +1,14 @@
+//
+//  CoreBluetoothWrapper.swift
+//  CoreBluetoothWrapper
+//
+//  Created by Sergey Pohrebnuak on 17.06.2021.
+//
+
+import UIKit
+
+public struct Printer {
+    static func printText(_ text: String) {
+        print("✅ this text printed from pod", text)
+    }
+}
